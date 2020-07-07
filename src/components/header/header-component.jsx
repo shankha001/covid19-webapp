6f359a8526b8bbfe08c6a03ca52f92b25de94913
shankha001 <59788600+shankha001@users.noreply.github.com>
2020-07-07 22:27:11 +0530

    make header responsive

--- src/components/header/header-component.jsx
+++ src/components/header/header-component.jsx
@@ -3,34 +3,56 @@ import './header.styles.scss';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/virus.svg';
 
-const Header = () => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link className="option" to="/">
-        <i className="fas fa-home icon"></i>
-        Home
-      </Link>
-      <Link className="option" to="/world">
-        <i className="fas fa-globe icon"></i>
-        World
-      </Link>
-      <Link className="option" to="/india">
-        <img
-          className="icon "
-          src={require('../../assets/images/india.png')}
-          alt="hello"
-        />
-        India
-      </Link>
-      <Link className="option" to="/admin">
-        <i className="fas fa-user-cog icon"></i>
-        Login
-      </Link>
-    </div>
-  </div>
-);
+function Header(props) {
+  const handleClick = () => {
+    document.querySelector('ul').classList.toggle('active');
+    document.querySelector('header').classList.toggle('margin-big');
+  };
+  return (
+    <React.Fragment>
+      <header className="header">
+        <Link className="logo-container" to="/">
+          <Logo className="logo" />
+        </Link>
+
+        <div>
+          <div className="toggle" onClick={handleClick}>
+            <i class="fas fa-bars menu"></i>
+          </div>
+          <ul className="options">
+            <li>
+              <Link className="option" to="/">
+                <i className="fas fa-home icon"></i>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="option" to="/world">
+                <i className="fas fa-globe icon"></i>
+                World
+              </Link>
+            </li>
+            <li>
+              <Link className="option" to="/india">
+                <img
+                  className="icon "
+                  src={require('../../assets/images/india.png')}
+                  alt="hello"
+                />
+                India
+              </Link>
+            </li>
+            <li>
+              <Link className="option" to="/admin">
+                <i className="fas fa-user-cog icon"></i>
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </React.Fragment>
+  );
+}
 
 export default Header;
