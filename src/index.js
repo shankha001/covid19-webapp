1e920f416538a92846cb8c503b4d8e6e2463f99a
shankha001 <shankhasubhrabiswas@gmail.com>
2020-10-03 11:53:45 +0530

    scrolltotop on new page

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Scrolltotop from './scrolltotop';
 
 ReactDOM.render(
   <BrowserRouter>
+    <Scrolltotop />
     <App />
   </BrowserRouter>,
   document.getElementById('root')
